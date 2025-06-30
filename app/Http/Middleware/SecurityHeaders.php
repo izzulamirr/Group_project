e2d443bbb8e66b95e7d4b8b8e432e88b44c159ff
izzulamirr <izzulamir.zulkifly@live.iium.edu.my>
2025-07-01 02:39:38 +0800

    Update SecurityHeaders.php

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -12,7 +12,11 @@ class SecurityHeaders
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; style-src 'self' https://cdn.jsdelivr.net https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; img-src 'self' data:; object-src 'none'; frame-ancestors 'none'; form-action 'self';");
+       // Update Content-Security-Policy header with nonce
+        $response->headers->set(
+            'Content-Security-Policy',
+            "default-src 'self'; script-src 'self' 'nonce-{$nonce}' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; style-src 'self' 'nonce-{$nonce}' https://cdn.jsdelivr.net https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; object-src 'none'; frame-ancestors 'none'; form-action 'self';"
+        );
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Access-Control-Allow-Origin', 'https://localhost');
